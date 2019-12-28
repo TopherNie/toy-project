903936fb008555d00af08b4354dc0a68846dc6ff
TopherNie <nxt15574917628>
2019-12-28 19:33:22 +0800

    room and player

---
+++ CFR/zhajinhua/include/player.h
@@ -0,0 +1,8 @@
+//
+// Created by Administrator on 2019/12/28 0028.
+//
+
+#ifndef TOY_PROJECT_PLAYER_H
+#define TOY_PROJECT_PLAYER_H
+
+#endif //TOY_PROJECT_PLAYER_H
---
+++ CFR/zhajinhua/src/room.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Administrator on 2019/12/28 0028.
+//
+
